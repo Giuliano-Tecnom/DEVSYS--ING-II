cdaa55f8a9f179e0eff805377bdea338d68afe84
Giuliano Piazzese <giuliano.piazzese@tecnom.com.ar>
2013-05-28 16:36:20 -0300

    db
    data base.

---
+++ db/clinicsystem.sql
@@ -0,0 +1,134 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.8.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 28, 2013 at 09:23 PM
+-- Server version: 5.6.11-log
+-- PHP Version: 5.4.14
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `clinicsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `especialidades`
+--
+
+CREATE TABLE IF NOT EXISTS `especialidades` (
+  `idespecialidad` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`idespecialidad`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `horarios`
+--
+
+CREATE TABLE IF NOT EXISTS `horarios` (
+  `idhorario` int(11) NOT NULL AUTO_INCREMENT,
+  `horaIn` time NOT NULL,
+  `horaOut` time NOT NULL,
+  `dia` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`idhorario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medicos`
+--
+
+CREATE TABLE IF NOT EXISTS `medicos` (
+  `nrolicencia` int(11) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `apellido` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `fechaNac` date NOT NULL,
+  `dni` int(11) NOT NULL,
+  `calle` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `numerocasa` int(11) NOT NULL,
+  `piso` int(11) NOT NULL,
+  `depto` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`nrolicencia`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `obrasociales`
+--
+
+CREATE TABLE IF NOT EXISTS `obrasociales` (
+  `idobra` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`idobra`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pacientes`
+--
+
+CREATE TABLE IF NOT EXISTS `pacientes` (
+  `idpaciente` int(11) NOT NULL AUTO_INCREMENT,
+  `dni` int(11) NOT NULL,
+  `apellido` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `fechaNac` date NOT NULL,
+  `calle` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `numerocasa` int(11) NOT NULL,
+  `piso` int(11) NOT NULL,
+  `depto` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`idpaciente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `turnos`
+--
+
+CREATE TABLE IF NOT EXISTS `turnos` (
+  `idturno` int(11) NOT NULL AUTO_INCREMENT,
+  `hora` time NOT NULL,
+  `fecha` date NOT NULL,
+  PRIMARY KEY (`idturno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `apellido` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `usuario` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `tipo` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
