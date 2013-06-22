b72a772a5024698cf591b542ce1a911714ab4584
Esteban Roberto Salinas <estebansalinas91@gmail.com>
2013-06-22 18:38:51 -0300

    base de datos
    base

--- db/clinicsystem.sql
+++ db/clinicsystem.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-06-2013 a las 19:34:24
+-- Tiempo de generación: 22-06-2013 a las 23:37:55
 -- Versión del servidor: 5.6.11-log
 -- Versión de PHP: 5.4.14
 
@@ -51,26 +51,28 @@ CREATE TABLE IF NOT EXISTS `horarios` (
   `idhorario` int(11) NOT NULL AUTO_INCREMENT,
   `horaIn` time NOT NULL,
   `horaOut` time NOT NULL,
-  `dia` varchar(9) COLLATE utf8_unicode_ci NOT NULL,
+  `dia` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `dia_nro` int(2) NOT NULL,
+  `fecha` date NOT NULL,
   PRIMARY KEY (`idhorario`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=23 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=12 ;
 
 --
 -- Volcado de datos para la tabla `horarios`
 --
 
-INSERT INTO `horarios` (`idhorario`, `horaIn`, `horaOut`, `dia`) VALUES
-(1, '08:00:00', '14:00:00', 'Lunes   '),
-(2, '14:00:00', '20:00:00', 'Lunes   '),
-(3, '08:00:00', '14:00:00', 'Martes   '),
-(4, '14:00:00', '20:00:00', 'Martes   '),
-(5, '08:00:00', '14:00:00', 'Miercoles'),
-(6, '14:00:00', '20:00:00', 'Miercoles'),
-(7, '08:00:00', '14:00:00', 'Jueves  '),
-(8, '14:00:00', '20:00:00', 'Jueves  '),
-(9, '08:00:00', '14:00:00', 'Viernes  '),
-(10, '14:00:00', '20:00:00', 'Viernes  '),
-(11, '08:00:00', '14:00:00', 'Sabado  ');
+INSERT INTO `horarios` (`idhorario`, `horaIn`, `horaOut`, `dia`, `dia_nro`, `fecha`) VALUES
+(1, '08:00:00', '14:00:00', 'Lunes', 1, '2013-06-24'),
+(2, '14:00:00', '20:00:00', 'Lunes', 1, '2013-06-24'),
+(3, '08:00:00', '14:00:00', 'Martes', 2, '2013-06-25'),
+(4, '14:00:00', '20:00:00', 'Martes', 2, '2013-06-25'),
+(5, '08:00:00', '14:00:00', 'Miercoles', 3, '2013-06-26'),
+(6, '14:00:00', '20:00:00', 'Miercoles', 3, '2013-06-26'),
+(7, '08:00:00', '14:00:00', 'Jueves', 4, '2013-06-27'),
+(8, '14:00:00', '20:00:00', 'Jueves', 4, '2013-06-27'),
+(9, '08:00:00', '14:00:00', 'Viernes', 5, '2013-06-28'),
+(10, '14:00:00', '20:00:00', 'Viernes', 5, '2013-06-28'),
+(11, '08:00:00', '14:00:00', 'Sabado', 6, '2013-06-22');
 
 -- --------------------------------------------------------
 
@@ -90,15 +92,17 @@ CREATE TABLE IF NOT EXISTS `medicos` (
   `direccion` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `activo` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`idmedico`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
 
 --
 -- Volcado de datos para la tabla `medicos`
 --
 
 INSERT INTO `medicos` (`idmedico`, `nombre`, `apellido`, `nromatricula`, `email`, `telefono`, `fechaNac`, `dni`, `direccion`, `activo`) VALUES
-(1, 'Orlando', 'Piazzesi', 123456, 'osvaldo@gmail.com', 123456789, '1992-09-01', 123456789, '123456789', 1),
-(2, 'Javier', 'Mascherano', 12345633, '', 4356789, '2013-06-01', 12345678, '60 N 1009', 1);
+(1, 'Orlando', 'Piazzesi', 123456645, 'osvaldo@gmail.com', 123456789, '1992-09-01', 123456789, '123456789', 1),
+(2, 'Javier', 'Mascherano', 12345633, '', 4356789, '2013-06-01', 12345678, '60 N 1009', 1),
+(3, 'esteban', 'salinas', 1324342, '', 4847273, '2013-06-01', 364355435, '3434 nÂ° 343', 1),
+(4, 'pep', 'lalal', 1234568, '', 4838131, '2013-06-01', 345242342, '50 127', 1);
 
 -- --------------------------------------------------------
 
@@ -117,7 +121,12 @@ CREATE TABLE IF NOT EXISTS `med_esp` (
 --
 
 INSERT INTO `med_esp` (`idmedico`, `idespecialidad`) VALUES
-(2, 1);
+(1, 1),
+(1, 2),
+(2, 1),
+(3, 2),
+(4, 1),
+(4, 2);
 
 -- --------------------------------------------------------
 
@@ -136,10 +145,14 @@ CREATE TABLE IF NOT EXISTS `med_hor` (
 --
 
 INSERT INTO `med_hor` (`idmedico`, `idhorario`) VALUES
-(2, 3),
-(2, 6),
-(2, 7),
-(2, 10);
+(1, 1),
+(3, 1),
+(3, 3),
+(3, 5),
+(3, 11),
+(4, 1),
+(4, 4),
+(4, 8);
 
 -- --------------------------------------------------------
 
@@ -158,9 +171,14 @@ CREATE TABLE IF NOT EXISTS `med_obrasocial` (
 --
 
 INSERT INTO `med_obrasocial` (`idmedico`, `idobra`) VALUES
-(1, 1),
+(1, 2),
+(1, 3),
 (2, 1),
-(2, 2);
+(2, 2),
+(3, 1),
+(3, 2),
+(4, 1),
+(4, 2);
 
 -- --------------------------------------------------------
 
@@ -173,7 +191,7 @@ CREATE TABLE IF NOT EXISTS `obrasociales` (
   `nombre` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `activo` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`idobra`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 --
 -- Volcado de datos para la tabla `obrasociales`
@@ -181,7 +199,8 @@ CREATE TABLE IF NOT EXISTS `obrasociales` (
 
 INSERT INTO `obrasociales` (`idobra`, `nombre`, `activo`) VALUES
 (1, 'GALEN', 1),
-(2, 'OSDE', 1);
+(2, 'OSDE', 1),
+(3, 'IOMA', 1);
 
 -- --------------------------------------------------------
 
@@ -207,12 +226,12 @@ CREATE TABLE IF NOT EXISTS `pacientes` (
 --
 
 INSERT INTO `pacientes` (`idpaciente`, `dni`, `apellido`, `nombre`, `email`, `telefono`, `fechaNac`, `direccion`, `activo`) VALUES
-(73, 10000000, 'Piazzese', 'Esteban', 'giuly.tolosaa@hotmail.com', 4710301, '2013-06-01', '501 y 27', 0),
-(74, 36936534, 'Piazzese', 'Estebanquito', 'giuly.tolosaa@hotmail.com', 4710301, '2013-06-01', '501 y 27', 0),
+(73, 10000000, 'Piazzese', 'Esteban', 'giuly.tolosaa@hotmail.com', 4710301, '2013-06-01', '501 y 27', 1),
+(74, 36936534, 'Piazzese', 'Estebanquito', 'giuly.tolosaa@hotmail.com', 4710301, '2013-06-01', '501 y 27', 1),
 (75, 11221112, 'gonzalez', 'aaaaaaa', 'ezequiel@hotmail.com', 1234565687, '2013-06-01', '521 N 1009', 1),
 (76, 35179786, 'gonzalez', 'ezequiel', '', 2147483647, '2013-05-27', '18327824782', 1),
 (77, 534543535, 'dfgdgfdfg', 'fgdfgfg', '', 2147483647, '2013-06-01', 'fgdfgdfgdg', 1),
-(78, 111222333, 'prueba', 'pruebamodal', '', 111222333, '1939-02-15', 'concha de la lora', 0),
+(78, 111222333, 'prueba', 'pruebamodal', '', 111222333, '1939-02-15', 'concha de la lora', 1),
 (79, 432154321, 'maradona', 'Radiologia', '', 2147483647, '2013-06-01', '1wrqweqw', 1),
 (80, 444444444, 'safsaf', 'Esteban', 'gapiazzese@hotmail.com', 2147483647, '2013-05-29', '521 N 1009', 1),
 (81, 34567567, 'Piazzese', 'Luis', '', 123456789, '2013-06-01', 'la plata', 1);
@@ -235,6 +254,8 @@ CREATE TABLE IF NOT EXISTS `pac_obrasocial` (
 --
 
 INSERT INTO `pac_obrasocial` (`idpaciente`, `idobra`, `nroAfiliado`) VALUES
+(73, 2, '1245'),
+(74, 3, '1234'),
 (81, 1, '1234a');
 
 -- --------------------------------------------------------
