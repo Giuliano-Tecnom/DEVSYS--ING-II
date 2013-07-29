ad86de85b896182ebbc82500f84f93f8b906a40e
Esteban Salinas <estebansalinas91@gmail.com>
2013-07-29 19:56:16 -0300

    base de datos
    base de datos actualizada

--- db/clinicsystem (2).sql
+++ db/clinicsystem (2).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-07-2013 a las 23:00:09
+-- Tiempo de generación: 30-07-2013 a las 00:54:11
 -- Versión del servidor: 5.6.11-log
 -- Versión de PHP: 5.4.14
 
@@ -107,17 +107,17 @@ CREATE TABLE IF NOT EXISTS `horarios` (
 --
 
 INSERT INTO `horarios` (`idhorario`, `horaIn`, `horaOut`, `dia`, `dia_nro`, `fecha`) VALUES
-(1, '08:00:00', '14:00:00', 'Lunes', 1, '2013-07-22'),
-(2, '14:00:00', '20:00:00', 'Lunes', 1, '2013-07-22'),
-(3, '08:00:00', '14:00:00', 'Martes', 2, '2013-07-23'),
-(4, '14:00:00', '20:00:00', 'Martes', 2, '2013-07-23'),
-(5, '08:00:00', '14:00:00', 'Miercoles', 3, '2013-07-24'),
-(6, '14:00:00', '20:00:00', 'Miercoles', 3, '2013-07-24'),
-(7, '08:00:00', '14:00:00', 'Jueves', 4, '2013-07-18'),
-(8, '14:00:00', '20:00:00', 'Jueves', 4, '2013-07-18'),
-(9, '08:00:00', '14:00:00', 'Viernes', 5, '2013-07-19'),
-(10, '14:00:00', '20:00:00', 'Viernes', 5, '2013-07-19'),
-(11, '08:00:00', '14:00:00', 'Sabado', 6, '2013-07-20');
+(1, '08:00:00', '14:00:00', 'Lunes', 1, '2013-07-29'),
+(2, '14:00:00', '20:00:00', 'Lunes', 1, '2013-07-29'),
+(3, '08:00:00', '14:00:00', 'Martes', 2, '2013-07-30'),
+(4, '14:00:00', '20:00:00', 'Martes', 2, '2013-07-30'),
+(5, '08:00:00', '14:00:00', 'Miercoles', 3, '2013-07-31'),
+(6, '14:00:00', '20:00:00', 'Miercoles', 3, '2013-07-31'),
+(7, '08:00:00', '14:00:00', 'Jueves', 4, '2013-08-01'),
+(8, '14:00:00', '20:00:00', 'Jueves', 4, '2013-08-01'),
+(9, '08:00:00', '14:00:00', 'Viernes', 5, '2013-08-02'),
+(10, '14:00:00', '20:00:00', 'Viernes', 5, '2013-08-02'),
+(11, '08:00:00', '14:00:00', 'Sabado', 6, '2013-08-03');
 
 -- --------------------------------------------------------
 
@@ -132,14 +132,15 @@ CREATE TABLE IF NOT EXISTS `licencias` (
   `fechaHasta` date NOT NULL,
   `estado` tinyint(4) NOT NULL DEFAULT '1',
   PRIMARY KEY (`idlicencia`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
 
 --
 -- Volcado de datos para la tabla `licencias`
 --
 
 INSERT INTO `licencias` (`idlicencia`, `idmedico`, `fechaDesde`, `fechaHasta`, `estado`) VALUES
-(17, 5, '2013-07-19', '2013-07-23', 1);
+(17, 5, '2013-07-19', '2013-07-23', 0),
+(18, 5, '2013-07-30', '2013-08-02', 1);
 
 -- --------------------------------------------------------
 
@@ -159,7 +160,7 @@ CREATE TABLE IF NOT EXISTS `medicos` (
   `direccion` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `activo` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`idmedico`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
 
 --
 -- Volcado de datos para la tabla `medicos`
@@ -168,7 +169,8 @@ CREATE TABLE IF NOT EXISTS `medicos` (
 INSERT INTO `medicos` (`idmedico`, `nombre`, `apellido`, `nromatricula`, `email`, `telefono`, `fechaNac`, `dni`, `direccion`, `activo`) VALUES
 (5, 'Esteban', 'Salinas', 111111111, '', 223459977, '1987-06-04', 10789456, '75 N 800', 1),
 (6, 'Enrique', 'Planes', 555553333, '', 4536666, '1975-01-30', 20785463, '65 N 1021', 1),
-(7, 'Albert', 'Rincolben', 557894645, '', 4699855, '1990-05-15', 151515151, '528 N 777', 1);
+(7, 'Albert', 'Rincolben', 557894645, '', 4699855, '1990-05-15', 151515151, '528 N 777', 0),
+(8, 'Santiago', 'Puricelli', 9472742, '', 2147483647, '2009-12-02', 316361881, '4 y 527', 1);
 
 -- --------------------------------------------------------
 
@@ -190,7 +192,9 @@ INSERT INTO `med_esp` (`idmedico`, `idespecialidad`) VALUES
 (5, 9),
 (6, 6),
 (7, 5),
-(7, 9);
+(7, 9),
+(8, 6),
+(8, 9);
 
 -- --------------------------------------------------------
 
@@ -228,7 +232,10 @@ INSERT INTO `med_hor` (`idmedico`, `idhorario`) VALUES
 (7, 8),
 (7, 9),
 (7, 10),
-(7, 11);
+(7, 11),
+(8, 3),
+(8, 5),
+(8, 9);
 
 -- --------------------------------------------------------
 
@@ -253,7 +260,8 @@ INSERT INTO `med_obrasocial` (`idmedico`, `idobra`) VALUES
 (5, 9),
 (6, 6),
 (7, 6),
-(7, 7);
+(7, 7),
+(8, 7);
 
 -- --------------------------------------------------------
 
@@ -305,7 +313,7 @@ CREATE TABLE IF NOT EXISTS `pacientes` (
 INSERT INTO `pacientes` (`idpaciente`, `dni`, `apellido`, `nombre`, `email`, `telefono`, `fechaNac`, `direccion`, `activo`) VALUES
 (79, 35179786, 'Gonzalez', 'Ezequiel', '', 4536110, '1990-03-26', '60 N 1007', 1),
 (81, 32155166, 'Romero', 'Cristian', '', 4511010, '1985-10-10', '501 N 120', 1),
-(82, 16178752, 'Cambiasso', 'Adolfo', '', 4202020, '1999-02-15', '125 N 300', 1);
+(82, 16178752, 'Cambiasso', 'Adolfo', '', 4202020, '1999-02-15', '125 N 300', 0);
 
 -- --------------------------------------------------------
 
@@ -348,14 +356,15 @@ CREATE TABLE IF NOT EXISTS `turnos` (
   `idespecialidad` int(11) NOT NULL,
   `estado` varchar(500) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'en espera',
   PRIMARY KEY (`idturno`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=34 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=35 ;
 
 --
 -- Volcado de datos para la tabla `turnos`
 --
 
 INSERT INTO `turnos` (`idturno`, `idmedico`, `idpaciente`, `idobra`, `idhora`, `fecha`, `idespecialidad`, `estado`) VALUES
-(33, 5, 81, 999, 18, '2013-07-18', 9, 'cancelado');
+(33, 5, 81, 999, 18, '2013-07-18', 9, 'cancelado'),
+(34, 5, 79, 999, 1, '2013-07-26', 9, 'en espera');
 
 -- --------------------------------------------------------
 
@@ -371,7 +380,15 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `tipo` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`idusuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`idusuario`, `nombre`, `apellido`, `usuario`, `password`, `tipo`) VALUES
+(3, 'ESTEBAN', 'SALINAS', 'SALINAS', '12345', 'administrador'),
+(6, 'CRISTIAN', 'ROMERO', 'ROMERO', '12345', 'usuario');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
